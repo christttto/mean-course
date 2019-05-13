6d032701398566a252d4a7251c5d8bc85d909a9a
Kris <christttto@berkeley.edu>
2019-05-12 23:42:18 -0700

    24

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from "@angular/core"
-
+import { Post } from "../post.model"
 @Component({
   selector: "app-post-list",
   templateUrl: "./post-list.component.html",
@@ -11,5 +11,5 @@ export class PostListComponent {
   //   { title: "Second Post", content: "This is a second post's content" },
   //   { title: "Third Post", content: "This is a third post's content" }
   // ]
-  @Input() posts = []
+  @Input() posts: Post[] = []
 }
