c3c6cc2818f7cc01f02c35087ccbc96e92e8984a
Kris <christttto@berkeley.edu>
2019-05-13 01:15:28 -0700

    solved

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,9 +1,6 @@
 import { Component, EventEmitter, Output } from "@angular/core"
 import { Post } from "../post.model"
-<<<<<<< HEAD
 import { NgForm } from "@angular/forms"
-=======
->>>>>>> 6d032701398566a252d4a7251c5d8bc85d909a9a
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html",
@@ -14,17 +11,10 @@ export class PostCreateComponent {
   enteredContent = ""
   @Output() postCreated = new EventEmitter<Post>()
 
-<<<<<<< HEAD
   onAddPost(form: NgForm) {
     const post: Post = {
       title: form.value.title,
       content: form.value.content
-=======
-  onAddPost() {
-    const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
->>>>>>> 6d032701398566a252d4a7251c5d8bc85d909a9a
     }
     this.postCreated.emit(post)
   }
