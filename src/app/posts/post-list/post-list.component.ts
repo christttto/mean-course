eec994e003566f7579f6a670da58dbaf74a0e129
Kris <christttto@berkeley.edu>
2019-05-12 20:49:10 -0700

    22

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,9 +6,10 @@ import { Component } from "@angular/core"
   styleUrls: ["./post-list.component.css"]
 })
 export class PostListComponent {
-  posts = [
-    { title: "First Post", content: "This is a first post's content" },
-    { title: "Second Post", content: "This is a second post's content" },
-    { title: "Third Post", content: "This is a third post's content" }
-  ]
+  // posts = [
+  //   { title: "First Post", content: "This is a first post's content" },
+  //   { title: "Second Post", content: "This is a second post's content" },
+  //   { title: "Third Post", content: "This is a third post's content" }
+  // ]
+  posts = []
 }
