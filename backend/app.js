c1965c0c9abb6b6aa9ac684683c1a65fed888a3f
Kris <christttto@berkeley.edu>
2019-05-16 23:50:28 -0700

    37

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,27 @@ const express = require("express")
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log("First middleware")
-  next()
-})
-
-app.use((req, res, next) => {
-  res.send("Hello from express!")
+//'/posts' will now make it only handle this path
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "fadf124211l",
+      title: "First server-side post",
+      content: "This is coming from the server"
+    },
+    {
+      id: "ksajflaj132",
+      title: "Second server-side post",
+      content: "This is coming from the server!"
+    }
+  ]
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts
+  })
+  //no next, because there is no middleware after this
 })
 
 module.exports = app
+
+//register path
--- src/app/posts/post.model.ts
+++ src/app/posts/post.model.ts
@@ -2,3 +2,4 @@ export interface Post {
   title: string
   content: string
 }
+//now there will be id
